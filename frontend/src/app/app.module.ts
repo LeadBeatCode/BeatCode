5295222e3284436859154198a0224c9404e0fe4d
somlv <somvelya99@gmail.com>
2024-06-26 23:04:03 -0400

    feat: integrate monaco code editor

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MonacoEditorModule.forRoot(), // use forRoot() in main app module only.
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
