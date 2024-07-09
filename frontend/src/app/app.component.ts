b596d97ce9c20be3a340190b9cb4fda8a181af68
Jin Ting Zhou <jinting.zhou@mail.utoronto.ca>
2024-07-09 10:50:24 -0400

    fix: apiService import path change

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ApiService } from './services/api.service';
+import { ApiService } from './services/apiService/api.service';
 
 declare const monaco: any;
 
