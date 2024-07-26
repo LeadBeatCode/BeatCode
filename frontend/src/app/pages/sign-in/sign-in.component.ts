382b174650654637a0edd99db24559d2c3a23156
Jin Ting Zhou <jinting.zhou@mail.utoronto.ca>
2024-07-26 14:04:45 -0400

    chore: removed unneeded function in sign-in

--- frontend/src/app/pages/sign-in/sign-in.component.ts
+++ frontend/src/app/pages/sign-in/sign-in.component.ts
@@ -103,31 +103,4 @@ export class SignInComponent implements OnInit {
       .subscribe((result) => console.log(result));
   }
 
-  randomString(length: number) {
-    const chars =
-      '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    let result = '';
-    for (let i = length; i > 0; --i) {
-      result += chars[Math.floor(Math.random() * chars.length)];
-    }
-    return result;
-  }
-  enterLobby() {
-    console.log('enterLobby');
-    // const username = this.randomString(8);
-    // const password = this.randomString(8);
-    // this.api.signUp(username, password).subscribe({
-    //   next:(data) => {
-    //     console.log(data);
-    //     const userId = data.id;
-    //     localStorage.setItem('userId', userId);
-    //     this.router.navigate(['/matching-lobby']);
-    //   },
-    //   error: (err) => {
-    //     console.log(err);
-    //   }
-
-    // });
-    this.router.navigate(['/matching-lobby']);
-  }
 }
