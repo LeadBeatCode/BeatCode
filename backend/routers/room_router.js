0bd6a72f0e92493ae2f23a02b1d944caa364bfdf
Jin Ting Zhou <jinting.zhou@mail.utoronto.ca>
2024-07-28 09:54:44 -0400

    fix: more files for quick match history

--- backend/routers/room_router.js
+++ backend/routers/room_router.js
@@ -1,13 +1,17 @@
 import { Router } from "express";
 import { Room } from "../models/room.js";
 import { User } from "../models/user.js";
-import { isAuthorizedRoom } from "../middleware/helper.js";
-import { isAuthenticated } from "../middleware/helper.js";
+import { isAuthorizedRoom, isAuthenticated, isAuthenticatedRoom } from "../middleware/helper.js";
 
 export const roomRouter = Router();
 
 roomRouter.post("/", isAuthenticated, async (req, res) => {
   try {
+    const user = await User.findOne({
+      where: {
+        id: req.body.userId1,
+      },
+    });
     const user2 = await User.findOne({
       where: {
         id: req.body.userId2,
@@ -16,7 +20,12 @@ roomRouter.post("/", isAuthenticated, async (req, res) => {
     const isPve = req.body.isPve;
     if (!user2 && !isPve)
       return res.status(404).json({ error: "User not found" });
+    console.log("logging isPve", isPve);
+    console.log("logging user1", req.body.userId1);
+    console.log("logging user2", req.body.userId2);
+    console.log("logging status", req.body.status);
     const room = await Room.create({
+      gameType: req.body.gameType,
       status: req.body.status,
       userId1: req.body.userId1,
       userId2: isPve ? "pveGame" : req.body.userId2,
@@ -24,6 +33,8 @@ roomRouter.post("/", isAuthenticated, async (req, res) => {
       user1Status: isPve ? "pve" : "pending",
       user2Status: isPve ? "pve" : "pending",
       questionTitleSlug: req.body.questionTitleSlug,
+      user1Nickname: user.nickname,
+      user2Nickname: isPve ? "pveGame" : user2.nickname,
     });
     console.log("logging room", room);
     return res.json(room);
@@ -32,7 +43,7 @@ roomRouter.post("/", isAuthenticated, async (req, res) => {
   }
 });
 
-roomRouter.get("/:id", async (req, res) => {
+roomRouter.get("/:id", isAuthenticated, async (req, res) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
     const room = await Room.findByPk(req.params.id);
@@ -42,10 +53,6 @@ roomRouter.get("/:id", async (req, res) => {
         accessToken: token,
       },
     });
-    if (!user) return res.status(404).json({ error: "User not found" });
-    if (user.id !== room.userId1 && user.id !== room.userId2) {
-      return res.status(403).json({ error: "Unauthorized" });
-    }
     const isPve = room.isPve;
     if (user.id === room.userId1) {
       console.log("logging room", room);
@@ -64,6 +71,10 @@ roomRouter.get("/:id", async (req, res) => {
         user1Status: room.user1Status,
         user2Status: room.user2Status,
         questionTitleSlug: room.questionTitleSlug,
+        gameType: room.gameType,
+        winner: room.winner,
+        user1Nickname: room.user1Nickname,
+        user2Nickname: room.user2Nickname
       });
     }
     console.log("logging room", room);
@@ -82,6 +93,10 @@ roomRouter.get("/:id", async (req, res) => {
       user1Status: room.user1Status,
       user2Status: room.user2Status,
       questionTitleSlug: room.questionTitleSlug,
+      gameType: room.gameType,
+      winner: room.winner,
+      user1Nickname: room.user1Nickname,
+      user2Nickname: room.user2Nickname
     });
   } catch (error) {
     return res.status(400).json({ error: error.message });
@@ -121,7 +136,7 @@ roomRouter.get("/:id/sockets", async (req, res) => {
   }
 });
 
-roomRouter.put("/:id/playerStatus", async (req, res) => {
+roomRouter.put("/:id/playerStatus", isAuthenticated, async (req, res) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
     const room = await Room.findByPk(req.params.id);
@@ -142,6 +157,19 @@ roomRouter.put("/:id/playerStatus", async (req, res) => {
       console.log("setting user2 status", req.body.status);
       room.user2Status = req.body.status;
     }
+    await room.save();
+    return res.json(room);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+});
+
+roomRouter.put("/gameover",  async (req, res) => {
+  try {
+    const room = await Room.findByPk(req.body.roomId);
+    if (!room) return res.status(404).json({ error: "Room not found" });
+    room.status = "gameover";
+    room.winner = req.body.winner;
     await room.save();
     return res.json(room);
   } catch (error) {
