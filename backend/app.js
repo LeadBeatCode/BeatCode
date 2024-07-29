d5389fc60705c73cc60d844214aeb4f96a54bee5
Jin Ting Zhou <jinting.zhou@mail.utoronto.ca>
2024-07-29 02:06:46 -0400

    fix: cors duplicated

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import bodyParser from "body-parser";
-import cors from "cors";
 import http from "https";
 import { auth } from "express-openid-connect";
 import { Server } from "socket.io";
