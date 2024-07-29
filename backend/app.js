f4e987a2db015854d1e53918106e385437172fab
somlv <somvelya99@gmail.com>
2024-07-29 17:27:01 -0400

    fix; bug

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,6 @@ import { roomRouter } from "./routers/room_router.js";
 import { problemRouter } from "./routers/problem_router.js";
 import { leetcodeRouter } from "./routers/leetcode_router.js";
 import { leetcodeQueueRouter } from "./routers/leetcodeQueue_router.js";
-import { leaderboardRouter } from "./routers/leaderboard_router.js";
 import { friendRouter } from "./routers/friend_router.js";
 import { apiService } from "./api-service.js";
 import dotenv from "dotenv";
@@ -21,9 +20,8 @@ const PORT = 3000;
 const socketPort = 3001;
 export const app = express();
 
-
+const httpServer = http.createServer(app);
 app.use(bodyParser.json());
-app.use(express.static("static"));
 dotenv.config();
 
 try {
@@ -35,7 +33,7 @@ try {
 }
 
 const corsOptions = {
-  origin: ["https://beat.codes", "https://api.beat.codes", "http://localhost:4200"],
+  origin: "http://localhost:4200",
   credentials: true, //allows cookies and HTTP authentication information to be included in the requests sent to the server
 };
 app.use(cors(corsOptions));
@@ -52,12 +50,11 @@ app.use("/api/leetcode", leetcodeRouter);
 app.use("/api/friends", friendRouter);
 app.use("/api/problems", problemRouter);
 app.use("/api/leetcodeQueues", leetcodeQueueRouter);
-app.use("/api/leaderboard", leaderboardRouter);
-const httpServer = http.createServer(app);
+
 export const io = new Server(httpServer, {
   cors: {
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    origin: ["https://beat.codes", "https://api.beat.codes", "http://localhost:4200"],
+    origin: "http://localhost:4200",
+    methods: ["GET", "POST"],
     credentials: true,
   },
 });
@@ -67,29 +64,54 @@ export const io = new Server(httpServer, {
 io.on("connection", (socket) => {
   
   console.log("a user connected");
+  // console.log("socket.handshake.headers", socket.handshake.headers);
 
   socket.on("reconnected", function (userId, accessToken) {
+    // console.log(nickname, accessToken);
+    // apiService.getUserSocketId(nickname).then((res) => {
+    //   apiService.setUserSocket(socket.id, nickname, userId, accessToken).then((res) => {
+    //     console.log('setUserSocket', res);
+    //   });
+    // });
     console.log("sdfsdf", userId)
     socket.emit("reconnected", userId, socket.id);
   });
+  // socket.on("roomSocket", (roomId) => {
+  //   socket.emit("roomSocket", sockets)
+  // })
 
    socket.on("opponent_reconnected", (roomId, title, socketId, toSocketId, token) => {
     console.log("opponent_reconnected", roomId, title);
         io.to(toSocketId).emit("opponent_reconnected", title, socketId, roomId);
+    // apiService.getRoom(roomId, token).then((res) => {
+    //   if (title === 'p1') 
+    //     socket.emit("opponent_recconected", title, res.socketId1, roomId);
+    //   else
+    //     socket.emit("opponent_recconected", title, res.socketId2, roomId);
+    // });
   })
+  
+
+  //  apiService.createProblem('Longest Substring Without Repeating Characters', 'longest-substring-without-repeating-characters', { 'subInput1':'abcabcbb'  }, {  "subOutput1": 3 }, { 'subInput1':'bbbbb'  }, {  "subOutput1": 1 }, { 'subInput1':'pwwkew'  }, {  "subOutput1": 3 }).then((res) => {
+  //    console.log('problem', res);
+  //  });
   const token = socket.handshake.query.token;
+  // console.log("token at 70", socket.handshake.query);
   socket.on("online", function (data) {
     const { userId, friendSocketId } = data;
+    // console.log("online", userId, friendSocketId);
     io.to(friendSocketId).emit("friend online", userId);
   });
 
   socket.on("new friend", function (data) {
     const { friendId, friendSocketId } = data;
+    // console.log("new friend", friendId, friendSocketId);
     io.to(friendSocketId).emit("new friend added", friendId);
   });
 
   socket.on("editor", function (data) {
     const { targetSocketId, code } = data;
+    // Using 'to' to emit to a specific socket by its ID
     io.to(targetSocketId).emit("editor", { code });
   });
 
@@ -98,6 +120,7 @@ io.on("connection", (socket) => {
     console.log("change language", targetSocketId, language);
     io.to(targetSocketId).emit("opponent change language", { language });
   });
+  // const token = 'Bearer ' + socket.handshake.headers.authorization;
   socket.on("matching", async function (userId, accessToken, gameType) {
     // Make this function async
     console.log("matching", userId);
@@ -279,7 +302,7 @@ io.on("connection", (socket) => {
   });
 
   // const userId = socket.handshake.headers.userId;
-  // console.log("token at 231", token);
+  console.log("token at 231", token);
   socket.on("disconnect", () => {
     console.log("user disconnected");
     apiService.deleteQueue(socket.id, token).then((res) => {
@@ -310,7 +333,7 @@ const config = {
   authRequired: false,
   auth0Logout: true,
   secret: process.env.SECRET,
-  baseURL: process.env.BASE_URL_FRONTEND,
+  baseURL: "http://localhost:3000",
   clientID: "Kmosk0ISBss1diEABRcTzKJwNceZpSqn",
   issuerBaseURL: "https://dev-jqe0hc4zidat2q1z.us.auth0.com",
 };
@@ -325,11 +348,11 @@ app.get("/profile", (req, res) => {
 
 app.get("/connect", (req, res) => res.oidc.login({ returnTo: "/sign-in" }));
 
-// app.listen(PORT, (err) => {
-//   if (err) console.log(err);
-//   else console.log("HTTP server on hhttps://beat.codes:%s", PORT);
-// });
+app.listen(PORT, (err) => {
+  if (err) console.log(err);
+  else console.log("HTTP server on http://localhost:%s", PORT);
+});
 
-httpServer.listen(PORT, () => {
-  console.log("server on https://beat.codes:%s", PORT);
+httpServer.listen(socketPort, () => {
+  console.log("Socket server on http://localhost:%s", socketPort);
 });
